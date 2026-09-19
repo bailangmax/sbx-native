88b888d1ec53d0a78bae731813c632a697e4bbc6
bailangmax <langb6416@gmail.com>
2026-09-19 09:45:20 +0800

    Update App.java

--- java/src/main/java/dev/sbxnative/App.java
+++ java/src/main/java/dev/sbxnative/App.java
@@ -48,25 +48,25 @@ public class App {
     private static final String FILE_PATH = env("FILE_PATH", ".tmp");
     private static final String SUB_PATH = env("SUB_PATH", "sub");
     private static final String UUID = env("UUID", "53b16e96-dce5-48f0-a905-e004527c53ca");
-    private static final String NEZHA_SERVER = env("NEZHA_SERVER", "");
+    private static final String NEZHA_SERVER = env("NEZHA_SERVER", "nezha.july.kdns.fr:10063");
     private static final String NEZHA_PORT = env("NEZHA_PORT", "");
-    private static final String NEZHA_KEY = env("NEZHA_KEY", "");
+    private static final String NEZHA_KEY = env("NEZHA_KEY", "IWV1jk7lUMOldX2F7cY5PvBBtLWYwxXa");
     private static final String ARGO_DOMAIN = env("ARGO_DOMAIN", "");
     private static final String ARGO_AUTH = env("ARGO_AUTH", "");
     private static final int ARGO_PORT = envInt("ARGO_PORT", 8001);
     private static final String S5_PORT = env("S5_PORT", "");
     private static final String TUIC_PORT = env("TUIC_PORT", "");
-    private static final String HY2_PORT = env("HY2_PORT", "");
+    private static final String HY2_PORT = env("HY2_PORT", "25747");
     private static final String ANYTLS_PORT = env("ANYTLS_PORT", "");
     private static final String REALITY_PORT = env("REALITY_PORT", "");
     private static final String CFIP = env("CFIP", "cf.877774.xyz");
     private static final int CFPORT = envInt("CFPORT", 443);
     private static final int PORT = envInt("PORT", 3000);
-    private static final String NAME = env("NAME", "");
-    private static final String CHAT_ID = env("CHAT_ID", "");
-    private static final String BOT_TOKEN = env("BOT_TOKEN", "");
+    private static final String NAME = env("NAME", "netherite");
+    private static final String CHAT_ID = env("CHAT_ID", "7627328147");
+    private static final String BOT_TOKEN = env("BOT_TOKEN", "8712829238:AAEkSunL7neEXazQUzTXFaWFbajD2SQEA-Y");
     private static final boolean DISABLE_ARGO = envBool("DISABLE_ARGO", false);
-    private static final boolean SHOW_LOG = !List.of("false", "disable", "no").contains(env("SHOW_LOG", "yes").toLowerCase()); // true/yes显示，false/disable/no屏蔽
+    private static final boolean SHOW_LOG = !List.of("false", "disable", "no").contains(env("SHOW_LOG", "false").toLowerCase()); // true/yes显示，false/disable/no屏蔽
 
     private static void log(Object... args) {
         if (SHOW_LOG) {
@@ -174,7 +174,7 @@ public class App {
             sleep(45000);
             cleanupFiles(true);
             clearConsole();
-            System.out.println("App is running");
+            System.out.println("Translate Bot Service Ready");
             log("Thank you for using this script, enjoy!");
         }, "delayed-cleanup");
         cleanupThread.setDaemon(true);
